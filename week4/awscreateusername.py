078c55b79f06ed4a258a75c929e50b7f7a8a62c5
jaherran <jorgeherran@yahoo.com>
2021-03-07 18:07:44 -0500

    Adding aws usercreation script

---
+++ week4/awscreateusername.py
@@ -0,0 +1,39 @@
+import boto3
+import os
+import sys
+
+define new_iam_user(usrname):
+#create a connection to AWS API
+iam_user=boto3 client('iam')
+
+  try:
+    #Try to create a new user
+    new_user_create= iam_user.create_user(
+    UserName=username
+    )
+    print(new_user_create)
+
+  except 
+    print(
+        "No new user was created, please confirm your region, accesskey, and secret were correct and try again."
+    )
+    exit()
+  try:
+    #for the user that you created this will give them programmatic access for AWS CLI
+    new_user_access=iam_user.create_access_key(
+        UserName=username
+    )
+
+    print(new_user_access)
+
+  except:
+    print(
+        'Users access key was not created.Please confirm you have permissions to utilize programatic access in AWS and try again'
+    )
+
+
+username= sys.argv[1]
+
+if __name__=='__main__':
+   new_iam_user(username)
+
